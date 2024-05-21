aa6a5da72b0f79a58c2c114bb62d28ad8e6bc729
Mathias Hilgert <matthiashilgertlol@gmail.com>
2024-05-21 13:33:19 -0300

    feat: :sparkles: Added DemoCasitaSimple01 and DemoCasitaPalabras01 pages

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
       input: {
         casitadigital: resolve(root, 'casitadigital/index.html'),
         casitacompleja: resolve(root, 'casitacompleja/index.html'),
+        
+        demoCasitaSimple01: resolve(root, 'demoCasitaSimple01/index.html'),
+        demoCasitaPalabras01: resolve(root, 'demoCasitaPalabras01/index.html'),
       },
     },
   },
