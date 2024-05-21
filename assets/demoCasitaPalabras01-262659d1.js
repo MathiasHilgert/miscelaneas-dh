5e1fa701f8a4d0ca79c1914c67542b23c6294c1a
Mathias Hilgert <matthiashilgertlol@gmail.com>
2024-05-21 13:33:45 -0300

    feat: :sparkles: Added DemoCasitaSimple01 and DemoCasitaPalabras01 pages

---
+++ assets/demoCasitaPalabras01-262659d1.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-ec808ebb.js";import{C as o}from"./casita-compleja-0a63a704.js";window.onload=()=>{o("ROMA")};
---
+++ assets/demoCasitaSimple01-721e01bd.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-ec808ebb.js";import{C as o}from"./casita-digital-7c469cd9.js";window.onload=()=>{o("F")};
