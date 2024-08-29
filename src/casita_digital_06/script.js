ced7247ea60c510720098f27a69df08f198a7e64
IsmaelAxel <ismael_callamullo@hotmail.com>
2024-08-29 14:15:03 -0300

    feat: new preview, and fix casita-compleja

--- src/casita_digital_06/script.js
+++ src/casita_digital_06/script.js
@@ -8,25 +8,33 @@ window.onload = () => {
         container: document.getElementById("casita"),
         preview: document.getElementById("preview"),
         onHouseChange: (houses) => {
-            // console.log(houses.index[1])
+            console.log(houses)
             houses.forEach(house => {
-                // Logica de casita compleja
+                const lettersPreview = document.querySelectorAll(".char-preview")[house.index - 1]
                 const indexContainer = document.createElement("div")
-                const indexNumber = document.createElement("span")
+                const indexNumber = document.createElement("b")
                 const htmlElement = house.htmlElement
                 indexNumber.innerHTML = house.index
+                const numbersPreview = indexNumber.cloneNode(true)
                 htmlElement.appendChild(indexContainer)
                 indexContainer.appendChild(indexNumber)
+                lettersPreview.appendChild(numbersPreview)
+                console.log(lettersPreview);
+                
                 if(house.isOK == false){
                     const check = document.createElement("span")
                     check.classList.add("check-false")
                     check.innerHTML = "❌"
+                    const checkPreview = check.cloneNode(true)
                     htmlElement.appendChild(check)
+                    lettersPreview.appendChild(checkPreview)
                 }else{
                     const check = document.createElement("span")
                     check.classList.add("check-true")
                     check.innerHTML = "✅"
+                    const checkPreview = check.cloneNode(true)
                     htmlElement.appendChild(check)
+                    lettersPreview.appendChild(checkPreview)
                 }
             }
         );
