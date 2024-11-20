b0f8aee00ce7f870f14acf299df8a5f0e7b37a8d
Mathias Hilgert <matthiashilgertlol@gmail.com>
2024-11-20 14:53:18 -0300

    fix: corregir la letra esperada en Casita Digital a "D"

--- src/casita_digital_E1/script.js
+++ src/casita_digital_E1/script.js
@@ -3,7 +3,7 @@ import CasitaDigital from "../shared/casita-digital";
 window.onload = () => {
     CasitaDigital({
         initialLetter: "D",
-        expectedLetter: "B",
+        expectedLetter: "D",
         house: document.getElementById("casa"),
         letter: document.getElementById("mensaje"),
     });
